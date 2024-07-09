f7a236dae7a03e0b847fc5f44582e976f78fbb9e
lirik <lirikcaka23@gmail.com>
2024-07-09 19:25:33 +0200

    Fixed problem

--- src/test/java/com/automation_project/step_definitions/LoginStepDefs.java
+++ src/test/java/com/automation_project/step_definitions/LoginStepDefs.java
@@ -2,43 +2,85 @@ package com.automation_project.step_definitions;
 
 import com.automation_project.pages.LoginPage;
 import com.automation_project.utilities.ConfigurationReader;
+import com.automation_project.utilities.Driver;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
+
 public class LoginStepDefs {
 
+    LoginPage loginPage = new LoginPage();
 
     @Given("the user is on the login page")
     public void the_user_is_on_the_login_page() {
-        System.out.println("Login to app in Before method");
+
     }
 
-    @Given("the user logged in as {string}")
-    public void the_user_logged_in_as(String userType) {
-        //based on input enter that user information
-        String username =null;
-        String password =null;
+    @When("user enters Human Resource username")
+    public void user_enters_human_resource_username() {
 
-        if(userType.equalsIgnoreCase("driver")){
-            username = ConfigurationReader.getProperty("driver_username");
-            password = ConfigurationReader.getProperty("driver_password");
-        }else if(userType.equalsIgnoreCase("sales manager")){
-            username = ConfigurationReader.getProperty("sales_manager_username");
-            password = ConfigurationReader.getProperty("sales_manager_password");
-        }else if(userType.equalsIgnoreCase("store manager")){
-            username = ConfigurationReader.getProperty("store_manager_username");
-            password = ConfigurationReader.getProperty("store_manager_password");
-        }
-        //send username and password and login
-        new LoginPage().login(username,password);
+        loginPage.userName.sendKeys(ConfigurationReader.getProperty("HR_username"));
+
     }
 
-    @Given("the user logged in with username as {string} and password as {string}")
-    public void the_user_logged_in_with_username_as_and_password_as(String username, String password) {
-      LoginPage loginPage=new LoginPage();
-      loginPage.login(username,password);
+    @When("user enters Human Resource password")
+    public void user_enters_human_resource_password() {
+        loginPage.password.sendKeys(ConfigurationReader.getProperty("HR_password") + Keys.ENTER);
+
+    }
+
+    @Then("user should see the dashboard")
+    public void user_should_see_the_dashboard() {
+
+    }
+
+    @When("user enters helpdesk username")
+    public void user_enters_helpdesk_username() {
+        loginPage.userName.sendKeys(ConfigurationReader.getProperty("Helpdesk_username"));
+
+    }
+
+    @When("user enters helpdesk password")
+    public void user_enters_helpdesk_password() {
+        loginPage.password.sendKeys(ConfigurationReader.getProperty("Helpdesk_password") + Keys.ENTER);
+
+
+    }
+
+    @When("user enters marketing username")
+    public void user_enters_marketing_username() {
+        loginPage.userName.sendKeys(ConfigurationReader.getProperty("Marketing_username"));
+
+
+    }
+
+    @When("user enters marketing password")
+    public void user_enters_marketing_password() {
+        loginPage.password.sendKeys(ConfigurationReader.getProperty("Marketing_password") + Keys.ENTER);
+
+
     }
 
 
+    @When("user logged in with username as {string} and password as {string}")
+    public void userLoggedInWithUsernameAsAndPasswordAs(String username, String password) {
 
+        loginPage.userName.sendKeys(username);
+        loginPage.password.sendKeys(password);
+        loginPage.loginButton.click();
+
+    }
+
+    @Then("user should be able to see error message {string}")
+    public void userShouldBeAbleToSeeErrorMessage(String expectedMessage) {
+
+        loginPage.errorMessage.isDisplayed();
+
+        String actualMessage = loginPage.errorMessage.getText();
+        Assert.assertEquals(actualMessage,expectedMessage);
+    }
 
 
 }
